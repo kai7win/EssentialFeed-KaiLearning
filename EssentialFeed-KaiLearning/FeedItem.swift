6b094601d625435a505a254ebfa4c072c49f5aa0
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-02-01 14:32:42 +0800

    Create FeedItem

---
+++ EssentialFeed-KaiLearning/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed-KaiLearning
+//
+//  Created by Thomas on 2023/2/1.
+//
+
+import Foundation
+
+struct FeedItem{
+    let id:UUID
+    let description:String?
+    let location:String?
+    let imageURL:URL
+}
