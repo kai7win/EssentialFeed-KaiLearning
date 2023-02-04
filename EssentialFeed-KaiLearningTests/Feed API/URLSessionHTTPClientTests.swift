8165f22a0b8610e92e8e6bf6ef76d9e2773541e3
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-02-05 06:52:34 +0800

    Delivers unexpected values error when request completes with data, response and error as nil

--- EssentialFeed-KaiLearningTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed-KaiLearningTests/Feed API/URLSessionHTTPClientTests.swift
@@ -16,10 +16,14 @@ class URLSessionHTTPClient{
         self.session = session
     }
     
+    struct UnexpectedValueRepresentation:Error{}
+    
     func get(from url:URL,completion:@escaping (HTTPClientResult) -> Void){
         session.dataTask(with: url) { _,_,error in
             if let error = error {
                 completion(.failure(error))
+            }else {
+                completion(.failure(UnexpectedValueRepresentation()))
             }
         }.resume()
     }
@@ -65,6 +69,27 @@ class URLSessionHTTPClientTests:XCTestCase{
                 XCTAssertNotNil(receivedError)
             default:
                 XCTFail("Expected failure with error \(error),got \(result) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    
+    func test_getFromURL_failsOnAllNillValues(){
+        
+        URLProtocolStub.stub(data:nil,response:nil,error:nil)
+
+        let exp = expectation(description: "wait for completion")
+        
+        makeSut().get(from:anyURL()){ result in
+            switch result {
+            case .failure:
+               break
+            default:
+                XCTFail("Expected failure ,got \(result) instead")
             }
             
             exp.fulfill()
