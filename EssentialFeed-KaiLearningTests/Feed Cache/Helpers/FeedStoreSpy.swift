b3e735ac18b476f00b217207246fe4e2b43abdbe
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-02-16 14:22:23 +0800

    Extract 'FeedStoreSpy' into a shared scope to remove duplication

---
+++ EssentialFeed-KaiLearningTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -0,0 +1,49 @@
+//
+//  FeedStoreSpy.swift
+//  EssentialFeed-KaiLearningTests
+//
+//  Created by Thomas on 2023/2/16.
+//
+
+import Foundation
+import EssentialFeed_KaiLearning
+
+class FeedStoreSpy:FeedStore{
+    
+    enum ReceivedMessage:Equatable{
+        case deleteCachedFeed
+        case insert([LocalFeedImage],Date)
+    }
+    
+    private(set) var receivedMessages = [ReceivedMessage]()
+    
+    private var deletionCompletions = [DeletionCompletion]()
+    private var insertionCompletions = [InsertionCompletion]()
+    
+    func deleteCachedFeed(comletion:@escaping DeletionCompletion){
+        deletionCompletions.append(comletion)
+        receivedMessages.append(.deleteCachedFeed)
+    }
+    
+    func completeDeletion(with error:Error,at index:Int = 0){
+        deletionCompletions[index](error)
+    }
+    
+    func completeDeletionSuccessfully(at index: Int = 0){
+        deletionCompletions[index](nil)
+    }
+    
+    func insert(_ items:[LocalFeedImage],timestamp:Date,completion:@escaping InsertionCompletion){
+        insertionCompletions.append(completion)
+        receivedMessages.append(.insert(items, timestamp))
+    }
+    
+    func completeInsertion(with error:Error,at index:Int = 0){
+        insertionCompletions[index](error)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0){
+        insertionCompletions[index](nil)
+    }
+    
+}
