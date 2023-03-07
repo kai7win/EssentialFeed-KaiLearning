4763025dad59c2f0e0577d8227e0bee961e11300
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-03-07 14:05:49 +0800

    Add ManagedCache and ManagedFeedImage model representations

---
+++ EssentialFeed-KaiLearning/Feed Cache/CoreDataFeedStore.swift
@@ -0,0 +1,38 @@
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeed-KaiLearning
+//
+//  Created by 湯瑪士 on 2023/3/7.
+//
+
+import CoreData
+
+public final class CoreDataFeedStore: FeedStore {
+    public init() {}
+
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+
+    }
+
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+
+    }
+
+}
+
+private class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+}
+
+private class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
+}
--- EssentialFeed-KaiLearningTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed-KaiLearningTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -6,14 +6,17 @@
 //
 
 import XCTest
+import EssentialFeed_KaiLearning
 
 class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        
+        let sut = makeSUT()
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        
+        let sut = makeSUT()
+        assertThatRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
     func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
@@ -56,5 +59,12 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
         
     }
     
+    // - MARK: Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
+        let sut = CoreDataFeedStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut as! FeedStore
+    }
     
 }
