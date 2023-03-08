770634385d4d74d5d29730fb81f752e6ddb6e498
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-03-08 09:09:55 +0800

    Extract duplicate code into a reusable helper method

--- EssentialFeed-KaiLearning/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed-KaiLearning/Feed Cache/CoreDataFeedStore.swift
@@ -17,8 +17,7 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        let context = self.context
-        context.perform {
+        perform { context in
             do {
                 if let cache = try ManagedCache.find(in: context) {
                     completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
@@ -32,8 +31,7 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        let context = self.context
-        context.perform {
+        perform { context in
             do {
                 let managedCache = try ManagedCache.newUniqueInstance(in: context)
                 managedCache.timestamp = timestamp
@@ -48,8 +46,7 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        let context = self.context
-        context.perform {
+        perform { context in
             do {
                 try ManagedCache.find(in: context).map(context.delete).map(context.save)
                 completion(nil)
@@ -58,6 +55,12 @@ public final class CoreDataFeedStore: FeedStore {
             }
         }
     }
+    
+    private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+        let context = self.context
+        context.perform { action(context) }
+    }
+    
 }
 
 
@@ -105,9 +108,9 @@ private class ManagedCache: NSManagedObject {
     }
     
     static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
-            try find(in: context).map(context.delete)
-            return ManagedCache(context: context)
-        }
+        try find(in: context).map(context.delete)
+        return ManagedCache(context: context)
+    }
     
     var localFeed: [LocalFeedImage] {
         return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
