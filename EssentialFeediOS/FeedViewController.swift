49f2d2c89a333136995585b1a9cfcfce5ef89326
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-03-11 18:13:10 +0800

    Cancel image URL preloading when image view is not near visible anymore

--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -69,7 +69,7 @@ final public class FeedViewController:UITableViewController, UITableViewDataSour
         cell.feedImageContainer.startShimmering()
         let loadImage = { [weak self, weak cell] in
             guard let self = self else { return }
-
+            
             self.tasks[indexPath] = self.imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
                 let data = try? result.get()
                 let image = data.map(UIImage.init) ?? nil
@@ -78,22 +78,29 @@ final public class FeedViewController:UITableViewController, UITableViewDataSour
                 cell?.feedImageContainer.stopShimmering()
             }
         }
-
+        
         cell.onRetry = loadImage
         loadImage()
         return cell
     }
     
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        tasks[indexPath]?.cancel()
-        tasks[indexPath] = nil
+        cancelTask(forRowAt: indexPath)
     }
     
     public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
-            indexPaths.forEach { indexPath in
-                let cellModel = tableModel[indexPath.row]
-                _ = imageLoader?.loadImageData(from: cellModel.url) { _ in }
-            }
+        indexPaths.forEach { indexPath in
+            let cellModel = tableModel[indexPath.row]
+            tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { _ in }
         }
+    }
     
+    public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
+        indexPaths.forEach(cancelTask)
+    }
+    
+    private func cancelTask(forRowAt indexPath: IndexPath) {
+        tasks[indexPath]?.cancel()
+        tasks[indexPath] = nil
+    }
 }
