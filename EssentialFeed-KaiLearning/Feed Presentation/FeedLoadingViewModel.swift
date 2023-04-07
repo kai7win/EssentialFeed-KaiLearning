a767dcec134ed632b3c2c25e7993f0bcd1872a81
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-04-08 07:14:21 +0800

    Move FeedLoadingViewModel to separate file

---
+++ EssentialFeed-KaiLearning/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed-KaiLearning
+//
+//  Created by Kai Chi Tsao on 2023/4/8.
+//
+
+import Foundation
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
