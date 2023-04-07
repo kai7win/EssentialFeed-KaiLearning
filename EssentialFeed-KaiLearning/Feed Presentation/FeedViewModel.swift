0f92c2252efaf8621b56850a45e972d92ac2dc1d
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-04-08 07:13:16 +0800

    Move FeedViewModel to separate file

---
+++ EssentialFeed-KaiLearning/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed-KaiLearning
+//
+//  Created by Kai Chi Tsao on 2023/4/8.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
