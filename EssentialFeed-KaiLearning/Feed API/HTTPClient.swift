6a566a252391b3e13a65656642f261da2e10913c
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-04-11 06:18:14 +0800

    Add HTTPClientTask so clients can cancel HTTP requests

--- EssentialFeed-KaiLearning/Feed API/HTTPClient.swift
+++ EssentialFeed-KaiLearning/Feed API/HTTPClient.swift
@@ -7,10 +7,16 @@
 
 import Foundation
 
+public protocol HTTPClientTask{
+    func cancel()
+}
+
 public protocol HTTPClient{
     typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+    
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch to appropriate threads, if needed.
     ///完成處理程序可以在任何線程中被調用。 如果需要，客戶端負責調度到適當的線程。
-    func get(from url:URL,completion:@escaping (Result) -> Void)
+    @discardableResult
+    func get(from url:URL,completion:@escaping (Result) -> Void) -> HTTPClientTask
 }
