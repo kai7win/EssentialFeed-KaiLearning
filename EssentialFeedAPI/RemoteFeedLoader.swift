6f82fe652bc652af10fd141e195ad21681f28c8b
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-04-24 12:51:04 +0800

    Move FeedImage mapping to the FeedItemsMapper

--- EssentialFeedAPI/RemoteFeedLoader.swift
+++ EssentialFeedAPI/RemoteFeedLoader.swift
@@ -41,7 +41,7 @@ public final class RemoteFeedLoader:FeedLoader{
     private static func map(_ data:Data,from response:HTTPURLResponse) -> Result{
         do{
             let items = try FeedItemsMapper.map(data, from: response)
-            return .success(items.toModels())
+            return .success(items)
         }catch{
             return .failure(error)
         }
@@ -51,10 +51,5 @@ public final class RemoteFeedLoader:FeedLoader{
 }
 
 
-private extension Array where Element == RemoteFeedItem{
-    func toModels() -> [FeedImage]{
-        return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.image)}
-    }
-}
 
 
