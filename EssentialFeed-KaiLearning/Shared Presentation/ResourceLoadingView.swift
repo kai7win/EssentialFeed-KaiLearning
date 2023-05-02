647a0e46ade640432e6d07fd185117f056e4ef19
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-05-03 06:38:40 +0800

    Rename and move ResourceLoadingView and VIewModel to Shared module

---
+++ EssentialFeed-KaiLearning/Shared Presentation/ResourceLoadingView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceLoadingView.swift
+//  EssentialFeed-KaiLearning
+//
+//  Created by Kai Chi Tsao on 2023/5/3.
+//
+
+import Foundation
+
+public protocol ResourceLoadingView {
+    func display(_ viewModel: ResourceLoadingViewModel)
+}
