c7b3d4d5840dac3c7f08a88c74fab71f8a6bd726
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-05-05 06:37:48 +0800

    Inline param

--- EssentialFeed-KaiLearningTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed-KaiLearningTests/Feed Presentation/FeedPresenterTests.swift
@@ -11,7 +11,7 @@ import EssentialFeed_KaiLearning
 class FeedPresenterTests: XCTestCase {
     
     func test_title_isLocalized() {
-            XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
+        XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
     }
     
     func test_map_createsViewModel() {
@@ -23,8 +23,9 @@ class FeedPresenterTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func localized(_ key: String, table: String = "Feed", file: StaticString = #file, line: UInt = #line) -> String {
-       
+    private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        
+        let table = "Feed"
         let bundle = Bundle(for: FeedPresenter.self)
         let value = bundle.localizedString(forKey: key, value: nil, table: table)
         if value == key {
