0e9536f9ce6eb9eee30f9ee0d169ca9bc0563232
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-05-16 13:49:04 +0800

    Add CellController protocol in preparation to support any cell controller type

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -12,14 +12,20 @@ public protocol FeedViewControllerDelegate{
     func didRequestFeedRefresh()
 }
 
+public protocol CellController{
+    func view(in tableView:UITableView) -> UITableViewCell
+    func preload()
+    func cancelLoad()
+}
+
 public final class FeedViewController:UITableViewController, UITableViewDataSourcePrefetching,ResourceLoadingView, ResourceErrorView{
     
     @IBOutlet private(set) public var errorView: ErrorView?
     
-    private var loadingControllers = [IndexPath: FeedImageCellController]()
+    private var loadingControllers = [IndexPath: CellController]()
     
     
-    private var tableModel = [FeedImageCellController]() {
+    private var tableModel = [CellController]() {
         didSet { tableView.reloadData() }
     }
     
@@ -41,7 +47,7 @@ public final class FeedViewController:UITableViewController, UITableViewDataSour
         delegate?.didRequestFeedRefresh()
     }
     
-    public func display(_ cellControllers: [FeedImageCellController]){
+    public func display(_ cellControllers: [CellController]){
         loadingControllers = [:]
         tableModel = cellControllers
     }
@@ -77,7 +83,7 @@ public final class FeedViewController:UITableViewController, UITableViewDataSour
         indexPaths.forEach(cancelCellControllerLoad)
     }
     
-    private func cellController(forRowAt indexPath: IndexPath) -> FeedImageCellController {
+    private func cellController(forRowAt indexPath: IndexPath) -> CellController {
         
         let controller = tableModel[indexPath.row]
         loadingControllers[indexPath] = controller
