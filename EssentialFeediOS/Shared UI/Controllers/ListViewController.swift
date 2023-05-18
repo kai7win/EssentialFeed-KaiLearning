6715c46c17d17e39575ecddbb8c97f765aef7ccd
KaiChi <34007164+kai7win@users.noreply.github.com>
2023-05-19 05:53:32 +0800

    Configure ErrorView programmatically so we don't duplicate layout logic in storyboards

--- EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -11,7 +11,7 @@ import EssentialFeed_KaiLearning
 
 public final class ListViewController:UITableViewController, UITableViewDataSourcePrefetching,ResourceLoadingView, ResourceErrorView{
     
-    @IBOutlet private(set) public var errorView: ErrorView?
+    private(set) public var errorView = ErrorView()
     
     private var loadingControllers = [IndexPath: CellController]()
     
@@ -24,9 +24,32 @@ public final class ListViewController:UITableViewController, UITableViewDataSour
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureErrorView()
         refresh()
     }
+    
+    private func configureErrorView() {
+        let container = UIView()
+        container.backgroundColor = .clear
+        container.addSubview(errorView)
+        
+        errorView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            errorView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+            container.trailingAnchor.constraint(equalTo: errorView.trailingAnchor),
+            errorView.topAnchor.constraint(equalTo: container.topAnchor),
+            container.bottomAnchor.constraint(equalTo: errorView.bottomAnchor),
+        ])
+        
+        tableView.tableHeaderView = container
+        
+        errorView.onHide = { [weak self] in
+            self?.tableView.beginUpdates()
+            self?.tableView.sizeTableHeaderToFit()
+            self?.tableView.endUpdates()
+        }
+    }
+    
     
     public override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -48,7 +71,7 @@ public final class ListViewController:UITableViewController, UITableViewDataSour
     }
     
     public func display(_ viewModel: ResourceErrorViewModel) {
-        errorView?.message = viewModel.message
+        errorView.message = viewModel.message
     }
     
     public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
